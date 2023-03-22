d1e2ca206f2ba360d1685ba0aaf9fa1837aa88ad
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-03-22 19:55:52 +0100

    method for roman numerals input check found

--- RomanNumeralsInputCheck/RomanNumeralsInputCheck/Program.cs
+++ RomanNumeralsInputCheck/RomanNumeralsInputCheck/Program.cs
@@ -3,6 +3,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<char> romanNumerals = new List<char> { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+        Console.WriteLine("Write in Roman numerals the edition of the book:");
+        string inputToCheck = Console.ReadLine();
+        List<char> userInput = inputToCheck.ToCharArray().ToList();
+        
+        var allOfListInputIsInRomanNumeralsList = userInput.Intersect(romanNumerals).Count() == userInput.Count();
+        Console.WriteLine(allOfListInputIsInRomanNumeralsList);
+        
     }
 }
