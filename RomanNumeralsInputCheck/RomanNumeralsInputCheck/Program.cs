6f3e1505d14915a633f86869dd6d4bd7aee885c4
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-03-23 15:28:15 +0100

    null or empty validation add

--- RomanNumeralsInputCheck/RomanNumeralsInputCheck/Program.cs
+++ RomanNumeralsInputCheck/RomanNumeralsInputCheck/Program.cs
@@ -6,10 +6,15 @@ class Program
         List<char> romanNumerals = new List<char> { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
         Console.WriteLine("Write in Roman numerals the edition of the book:");
         string inputToCheck = Console.ReadLine();
-        List<char> userInput = inputToCheck.ToCharArray().ToList();
-        
-        var allOfListInputIsInRomanNumeralsList = userInput.Intersect(romanNumerals).Count() == userInput.Count();
-        Console.WriteLine(allOfListInputIsInRomanNumeralsList);
-        
+        if (string.IsNullOrEmpty(inputToCheck))
+        {
+            Console.WriteLine("Invalid data");
+        }
+        else
+        {
+         List<char> userInput = inputToCheck.ToCharArray().ToList();
+         var allOfListInputIsInRomanNumeralsList = userInput.Intersect(romanNumerals).Count() == userInput.Count();
+         Console.WriteLine(allOfListInputIsInRomanNumeralsList);
+        }
     }
 }
